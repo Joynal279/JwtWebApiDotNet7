a8ef076342cdbecc8c91af9ece66e4f43a6ff860
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-08-19 23:49:46 +0600

    Login Post method implemented

--- JwtWebApiDotNet7/Controllers/AuthController.cs
+++ JwtWebApiDotNet7/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JwtWebApiDotNet7.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,9 @@ namespace JwtWebApiDotNet7.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+
+        public static User user = new User();
+
         // GET: api/Auth
         [HttpGet]
         public IEnumerable<string> Get()
@@ -26,9 +30,29 @@ namespace JwtWebApiDotNet7.Controllers
         }
 
         // POST: api/Auth
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
         {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            return Ok(user);
         }
 
         // PUT: api/Auth/5
